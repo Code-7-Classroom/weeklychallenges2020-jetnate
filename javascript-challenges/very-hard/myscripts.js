6d620443299b9f9ff011aa931869e7cd21695114
Nathan Yordanos Tadesse <natetadesse4991@gmail.com>
2020-03-30 20:23:13 -0400

    Added Project

--- javascript-challenges/very-hard/myscripts.js
+++ javascript-challenges/very-hard/myscripts.js
@@ -7,12 +7,26 @@ function addition(){return inputNum1 + inputNum2;}
 function multiply(){return inputNum1 * inputNum2;}
 function divide() {return inputNum1 / inputNum2;}
 
-if (operator == '+')  {
-    console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + addition ());}
-    else if (operator == '-')
-    { console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + subtract());}
-    else if (operator == '-')
-    { console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + multiply ());}
-    else if (operator == '/')
-    { console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + divide ());}
-    else {console.log( "That value is not valid" );}
+// if (operator == '+')  {
+//     console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + addition ());}
+//     else if (operator == '-')
+//     { console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + subtract());}
+//     else if (operator == '-')
+//     { console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + multiply ());}
+//     else if (operator == '/')
+//     { console.log (inputNum1 + ' ' + operator + ' ' + inputNum2 + '=' + divide ());}
+//     else {console.log( "That value is not valid" );}
+
+console.log(operator);
+
+if (operator == '+') {
+    console.log( 'user said +' ) ;
+} 
+
+else if (1==1) {
+
+    console.log('user said that value was true')
+}
+else {
+    console.log('The operator wasnt expected')
+}
