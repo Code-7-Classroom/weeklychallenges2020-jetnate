05a7b829c404d5295b012ce080e12b964c2b4cf9
Nathan Yordanos Tadesse <natetadesse4991@gmail.com>
2020-03-30 23:07:47 -0400

    added challenges

---
+++ javascript-challenges2/script.js
@@ -0,0 +1,23 @@
+/*Very Easy*/
+
+var min = function(a,b){
+    return (a<b)
+}
+
+
+
+/*Easy*/
+
+var firstname = ['john','nathan','james']
+var lastname = ['doe','tadesse','smith']
+var age = [20,15,40]
+
+
+console.log(' Hello,my name is ' + (firstname[1]) + " " + ( lastname[1] ) + ' and i am ' + (age[1]) + ' years old .');
+
+/*Medium*/
+
+var months = [ 'January' , 'February', 'March' , 'April' , 'May' + 'June' + 'July' + 'August' + 'September' + 'October' + 'November' + 'December'];
+
+
+
