b1ab04c7f027d5cb02587257d37dde0c1d4ef1f1
Nathan Yordanos Tadesse <natetadesse4991@gmail.com>
2020-04-13 17:05:49 -0400

    added checkpoint

---
+++ week10checkpoint/index.js
@@ -0,0 +1,18 @@
+// 1. Information is stored in an unordered set of pairs,values,or strings. They are enclosed in two curly brackets.
+
+//2.
+var me = {
+    property:me,
+    name:"Nathan Tadesse";
+    
+
+    
+}
+
+console.log(me)
+
+var sayHi = () => {
+    console.log("This is great!")
+}
+
+sayHi
